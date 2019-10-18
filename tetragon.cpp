8b744a4109ddc289c3a7db2a49106d96f9ac5a5f
paulpilipenko <paulpilipenko@gmail.com>
2019-10-18 15:13:28 +0300

    class tetragon работает. Багов не замечено.

--- tetragon.cpp
+++ tetragon.cpp
@@ -19,9 +19,11 @@ bool tetragon::TetragonOrNot(double distAB, double distBC, double distCD, double
 void tetragon::check(double distAB, double distBC, double distCD, double distAD, double area)
 {
 	bool rectangle = false;
+	double d;
+	d = distTo(_a, _c);
 	if ((distAB == distCD) && (distBC == distAD)) {
 		cout << "\nThis is parallelogram";
-		if (area == distAB * distBC) {
+		if ((area == distAB * distBC) && (d == pow(distAB,2) + pow(distBC,2))) {
 			cout << "\n...\nThis is rectangle";
 			rectangle = true;
 			if (area == distAB * distAB) {
@@ -34,12 +36,8 @@ void tetragon::check(double distAB, double distBC, double distCD, double distAD,
 			}
 		}
 	}
-	else {
-		double hAB_trapezoid = 2 * area / (distAB + distCD);
-		double hBC_trapezoid = 2 * area / (distBC + distAD);
-		if ((area == ((distAB + distCD) / 2) * hAB_trapezoid) || (area == ((distBC + distAD) / 2) * hBC_trapezoid)) {
-			cout << "\nThis is trapezoid";
-		}
+	else if ((d == sqrt(pow(distCD,2) + distAD*distBC - ((distAD*(pow(distCD,2) - pow(distAB,2)))/(distAD - distBC)))) || (d == sqrt(pow(distBC, 2) + distAB * distCD - ((distAB*(pow(distBC, 2) - pow(distAD, 2))) / (distAB - distCD))))) {
+		cout << "\nThis is trapezoid";
 	}
 }
 
@@ -62,11 +60,11 @@ tetragon::tetragon()
 		cout << "\nS = " << _S;
 		check(_ab, _bc, _cd, _ad, _S);
 	}
-	
-
 }
 
 
 tetragon::~tetragon()
 {
 }
+
+//(pow(d1, 2) + pow(d2, 2) == 2 * distAB*distCD + pow(distBC, 2) + pow(distAD, 2) || (pow(d1, 2) + pow(d2, 2) == 2 * distBC*distAD + pow(distAB, 2) + pow(distCD, 2)))
